0b11d178b30ed766780c5a4b73fc22c44e353202
febr3s <eduardofebresm@gmail.com>
2025-10-30 18:42:34 -0500

    eliminated all scripts. First commit before trying image_generate.py

---
+++ assets/env/src/image_generate .py
@@ -0,0 +1,11 @@
+# import sys
+# sys.path.append('scripts/')
+
+import field_updates # csv-process
+import replace_quotes # csv-process
+import copy_img # content-collections
+import blank_column # csv-process
+
+
+
+
